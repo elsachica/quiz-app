369e7b8dc17bb54be903829a83d9f772e85f8728
Elsa Gas Wikström <eg223ps@student.lnu.se>
2023-11-27 14:35:33 +0100

    Jag tycker att denna application borde funka

--- src/js/my-components/quiz-application/quiz-application.js
+++ src/js/my-components/quiz-application/quiz-application.js
@@ -80,5 +80,65 @@ class extends HTMLElement {
       this.startTimer()
     }
 
+    async fetchNextQuestion() {
+      try {
+        // Använd fetch API för att hämta nästa fråga från servern
+        const response = await fetch('https://courselab.lnu.se/quiz/question/1')
+        const data = await response.json()
+
+        // Uppdatera direkt frågekomponenten med frågan
+        this.shadowRoot.getElementById('question').question = data
+      } catch (error) {
+        // Om det uppstår ett fel, logga felet till konsolen
+        console.error('Error fetching question:', error)
+      }
+    }
+
+    async submitAnswer(answer) {
+      const requestBody = {
+        nickname: this.nickname,
+        answer: answer,
+      }
+
+      try {
+        const response = await fetch('https://courselab.lnu.se/quiz/question/1', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(requestBody),
+        })
+
+        const data = await response.json()
+
+        if (data.correct) {
+          // Om svaret är korrekt, hämta nästa fråga
+          this.fetchNextQuestion()
+        } else {
+          // Om svaret är fel, avsluta spelet
+          this.endGame()
+        }
+      } catch (error) {
+        console.error('Error submitting answer:', error)
+        return false
+      }
+    }
+
+    // startTimer () {
+    //   this.timerInterval = setInterval(() => {
+    //     this.timer--
+
+    //     if (this.timer <= 0) {
+    //       this.endGame()
+    //     }
+
+    //     this.shadowRoot.getElementById('timer').setAttribute('value', this.timer)
+    //   }, 1000)
+    // }
+
+    endGame () {
+      clearInterval(this.timerInterval)
+      this.shadowRoot.getElementById('highScore').updateHighScore(this.nickname, this.timer)
+    }
 })
 
